d8b236093b25f9d9f0efa6d81d6374f6e9d484ee
DevHyperCoder <arduinoleo88@gmail.com>
2020-08-24 15:45:10 +0530

    💄 Added defualt value to inputs

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
 <h2 className="heading">First Polynomial</h2>
       <div className="poly-container">
         <div className="poly">
-          <input type="number" name="p1-a" className="mathInput" id="p1-a" onChange={() => set_p1_a(document.getElementById('p1-a').value)} />
+          <input type="number" value={p1_a} name="p1-a" className="mathInput" id="p1-a" onChange={() => set_p1_a(document.getElementById('p1-a').value)} />
           <p>x²</p>
           <p>+</p>
-          <input type="number" name="p1-b" className="mathInput" id="p1-b" onChange={() => set_p1_b(document.getElementById('p1-b').value)} />
+          <input type="number" value={p1_b} name="p1-b" className="mathInput" id="p1-b" onChange={() => set_p1_b(document.getElementById('p1-b').value)} />
           <p>x</p>
           <p>+</p>
-          <input type="number" name="p1-c" className="mathInput" id="p1-c" onChange={() => set_p1_c(document.getElementById('p1-c').value)} />
+          <input type="number" value={p1_c} name="p1-c" className="mathInput" id="p1-c" onChange={() => set_p1_c(document.getElementById('p1-c').value)} />
           <p>=</p>
           <p>0</p>
         </div>
@@ -36,13 +36,13 @@ function App() {
 
       <div className="poly-container">
         <div className="poly">
-          <input type="number" name="p2-a" className="mathInput" id="p2-a" onChange={() => set_p2_a(document.getElementById('p2-a').value)} />
+          <input type="number" value={p2_a}  name="p2-a" className="mathInput" id="p2-a" onChange={() => set_p2_a(document.getElementById('p2-a').value)} />
           <p>x²</p>
           <p>+</p>
-          <input type="number" name="p2-b" className="mathInput" id="p2-b" onChange={() => set_p2_b(document.getElementById('p2-b').value)} />
+          <input type="number" value={p2_b}  name="p2-b" className="mathInput" id="p2-b" onChange={() => set_p2_b(document.getElementById('p2-b').value)} />
           <p>x</p>
           <p>+</p>
-          <input type="number" name="p2-c" className="mathInput" id="p2-c" onChange={() => set_p2_c(document.getElementById('p2-c').value)} />
+          <input type="number" value={p2_c}  name="p2-c" className="mathInput" id="p2-c" onChange={() => set_p2_c(document.getElementById('p2-c').value)} />
           <p>=</p>
           <p>0</p>
         </div>
@@ -51,13 +51,13 @@ function App() {
 
       <div className="poly-container">
         <div className="poly">
-          <input type="number" name="p-f-a" readOnly className="mathInput" id="p-f-a" value={calcPoly(operation, p1_a, p2_a)} />
+          <input type="number"  name="p-f-a" readOnly className="mathInput" id="p-f-a" value={calcPoly(operation, p1_a, p2_a)} />
           <p>x²</p>
           <p>+</p>
-          <input type="number" name="p-f-b" readOnly className="mathInput" id="p-f-b" value={calcPoly(operation, p1_b, p2_b)} />
+          <input type="number"  name="p-f-b" readOnly className="mathInput" id="p-f-b" value={calcPoly(operation, p1_b, p2_b)} />
           <p>x</p>
           <p>+</p>
-          <input type="number" name="p-f-c" readOnly className="mathInput" id="p-f-c" value={calcPoly(operation, p1_c, p2_c)} />
+          <input type="number"  name="p-f-c" readOnly className="mathInput" id="p-f-c" value={calcPoly(operation, p1_c, p2_c)} />
           <p>=</p>
           <p>0</p>
         </div>
